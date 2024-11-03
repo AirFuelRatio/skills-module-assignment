58ab0d03308fcf36202899f1ed5ee2a0721972f0
Tony Hong <tonyhong@Tonys-MacBook-Pro.local>
2024-11-03 16:13:59 -0700

    Updated print statement in merge-example branch

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # skills-module-assignment
 Basic Github repo to showcase skills I know related to Git.
+I updated this README on the merge-example branch I made!
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("This is an example program for CSE 485.")
+    print("I am changing this line in merge-example to create a merge issue!")
 
 if __name__ == "__main__":
     main()
